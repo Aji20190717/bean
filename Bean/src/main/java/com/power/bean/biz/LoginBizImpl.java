798d4f52b3fdf859c501a903b7dbfd57cdb8a0b3
Aji20190717 <gg3248@naver.com>
2020-12-08 21:58:13 +0900

    20201208

---
+++ Bean/src/main/java/com/power/bean/biz/LoginBizImpl.java
@@ -0,0 +1,30 @@
+package com.power.bean.biz;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.power.bean.dao.LoginDao;
+import com.power.bean.dto.LoginDto;
+
+@Service
+public class LoginBizImpl implements LoginBiz {
+	
+	@Autowired
+	private LoginDao dao;
+
+	@Override
+	public LoginDto login(LoginDto dto) {
+		return dao.login(dto);
+	}
+
+	@Override
+	public int resister(LoginDto dto) {
+		return dao.resister(dto);
+	}
+
+	@Override
+	public boolean idChk(String id) {
+		return dao.idChk(id);
+	}
+
+}
--- Bean/src/main/java/com/power/bean/controller/LoginController_Bean.java
+++ Bean/src/main/java/com/power/bean/controller/LoginController_Bean.java
@@ -1,5 +1,67 @@
 package com.power.bean.controller;
 
-public class LoginController_Bean {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.power.bean.biz.LoginBiz;
+
+@Controller
+public class LoginController_Bean {
+	
+	@Autowired
+	private LoginBiz biz;
+
+	// 메인 페이지에서 로그인/회원가입 버튼 누르면 오는 곳
+	@RequestMapping("/loginform.do")
+	public String loginform() {
+		
+		return "loginBean";
+		
+	}
+	
+	// 로그인/회원가입 페이지에서 회원가입 누르면 강사/수강생 선택 페이지로 감
+	@RequestMapping("/registtype.do")
+	public String registtype() {
+		
+		return "registType";
+		
+	}
+	
+	// 강사/수강생이 결정되면 값을 가지고 회원가입 폼으로 넘어감.
+	@RequestMapping("/registform.do")
+	public String registform(String type, HttpSession session) {
+		
+		session.setAttribute("type", type);
+		return "registBean";
+		
+	}
+	
+	// id체크하는 페이지
+	@RequestMapping("/idChk.do")
+	public String idChkpage(String id, Model model) {
+		
+		model.addAttribute("idChk", biz.idChk(id));
+		
+		return "idChk";
+		
+	}
+	
+	// email체크하는 페이지
+	@RequestMapping("/emailChk.do")
+	public String emailChkpage(String email, Model model) {
+			
+		model.addAttribute("emailChk", biz.idChk(email));
+			
+		return "meailChk";
+			
+	}
+	
+	
+	
+	
+	
 }
